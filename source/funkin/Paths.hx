052b9f232884270ea11fecd5278781c022e4a398
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-01-24 10:50:22 +0800

    Add files via upload

--- source/funkin/Paths.hx
+++ source/funkin/Paths.hx
@@ -52,6 +52,7 @@ class Paths
 		'assets/music/freakyMenu.$SOUND_EXT',
 		'assets/shared/music/breakfast.$SOUND_EXT',
 		'assets/shared/music/tea-time.$SOUND_EXT',
+		'assets/mobile/touchpad/bg.png'
 	];
 
 	/// haya I love you for the base cache dump I took to the max
@@ -78,6 +79,11 @@ class Paths
 		}
 		// run the garbage collector for good measure lmfao
 		System.gc();
+		#if cpp
+		cpp.NativeGc.run(true);
+		#elseif hl
+		hl.Gc.major();
+		#end
 		#if cpp
 		cpp.vm.Gc.compact();
 		#end
@@ -127,6 +133,9 @@ class Paths
 
 	public static function getPath(file:String, ?type:AssetType = TEXT, ?library:Null<String> = null)
 	{
+		if (library == "mobile")
+			return getSharedPath('mobile/$file');
+		
 		if (library != null) return getLibraryPath(file, library);
 
 		if (currentLevel != null)
@@ -506,7 +515,7 @@ class Paths
 	#if MODS_ALLOWED
 	inline static public function mods(key:String = '')
 	{
-		return 'content/' + key;
+		return #if mobile Sys.getCwd() + #end 'content/' + key;
 	}
 
 	inline static public function modsFont(key:String)
@@ -576,7 +585,7 @@ class Paths
 			var fileToCheck:String = mods(mod + '/' + key);
 			if (FileSystem.exists(fileToCheck)) return fileToCheck;
 		}
-		return 'content/' + key;
+		return #if mobile Sys.getCwd() + #end 'content/' + key;
 	}
 
 	public static var globalMods:Array<String> = [];
@@ -586,7 +595,7 @@ class Paths
 	static public function pushGlobalMods()
 	{ // prob a better way to do this but idc
 		globalMods = [];
-		if (FileSystem.exists("modsList.txt"))
+		if (FileSystem.exists(#if mobile Sys.getCwd() + #end "modsList.txt"))
 		{
 			var list:Array<String> = CoolUtil.listFromString(File.getContent("modsList.txt"));
 			for (i in list)
@@ -637,4 +646,25 @@ class Paths
 		return list;
 	}
 	#end
+	
+	public static function readDirectory(directory:String):Array<String>
+	{
+		#if MODS_ALLOWED
+		return FileSystem.readDirectory(directory);
+		#else
+		var dirs:Array<String> = [];
+		for (dir in Assets.list().filter(folder -> folder.startsWith(directory)))
+		{
+			@:privateAccess
+			for (library in lime.utils.Assets.libraries.keys())
+			{
+				if (library != 'default' && Assets.exists('$library:$dir') && (!dirs.contains('$library:$dir') || !dirs.contains(dir)))
+					dirs.push('$library:$dir');
+				else if (Assets.exists(dir) && !dirs.contains(dir))
+					dirs.push(dir);
+			}
+		}
+		return dirs.map(dir -> dir.substr(dir.lastIndexOf("/") + 1));
+		#end
+	}
 }
