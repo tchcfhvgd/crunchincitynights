3dfe18e0c9648b1d1132f982979e1d7a0818668f
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-01-27 11:28:12 +0800

    Add files via upload

---
+++ source/funkin/data/scripts/OverrideSubStateScript.hx
@@ -0,0 +1,29 @@
+package funkin.data.scripts;
+
+class OverrideSubStateScript extends FunkinIris
+{
+	public var customMenu:Bool = false;
+
+	public static function fromFile(file:String, ?name:String, ?additionalVars:Map<String, Any>)
+	{
+		if (name == null) name = file;
+
+		return new OverrideSubStateScript(File.getContent(file), name, additionalVars);
+	}
+
+	public function new(script:String, ?name:String = "Script", ?custom:Bool = true, ?additionalVars:Map<String, Any>)
+	{
+		super(script, name, additionalVars);
+
+		customMenu = call('customMenu', []);
+
+		trace('is [$name] custom? [$customMenu]');
+
+		set("state", flixel.FlxG.state);
+		set("close", FlxG.state.close);
+		set("add", FlxG.state.add);
+		set("remove", FlxG.state.remove);
+		set("insert", FlxG.state.insert);
+		set("members", FlxG.state.members);
+	}
+}
