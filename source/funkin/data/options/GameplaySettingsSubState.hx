c4b2ce4f0c8a532e83e92a368e55e79d6155c293
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-01-31 09:10:29 +0800

    Update GameplaySettingsSubState.hx

--- source/funkin/data/options/GameplaySettingsSubState.hx
+++ source/funkin/data/options/GameplaySettingsSubState.hx
@@ -11,8 +11,12 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 		title = 'Gameplay Settings';
 		rpcTitle = 'Gameplay Settings Menu'; // for Discord Rich Presence
 
+		var option:Option = new Option('Mechanics', 'Check this if you want to play with\nsome mechanics.',
+			'mechanics', 'bool', true);
+		addOption(option);
+		
 		var option:Option = new Option('Controller Mode', 'Check this if you want to play with\na controller instead of using your Keyboard.',
-			'controllerMode', 'bool', false);
+			'controllerMode', 'bool', true);
 		addOption(option);
 
 		// I'd suggest using "Downscroll" as an example for making your own option since it is the simplest here
